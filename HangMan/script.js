7934f2bf34b6b559a84fdb86b20d0d3c33ba5c14
Ba Van Phan <bavphan@gmail.com>
2016-10-17 14:25:09 -0400

    included if else loop logic

--- HangMan/script.js
+++ HangMan/script.js
@@ -3,32 +3,61 @@ $(document).ready(function(){
 
 var mysteryWord = {
   hangmanWord: [],
-  wordUnderscore: []
+  wordUnderscore: [],
+  wordArray: [],
+  counter: -1,
+  bodyParts: ["#headcenter", "#torsocenter", "#armleft", "#armright", "#legleft", "#legright"]
 }
+
+
+
 
 $("#submitInput").on("click", function(evt){
   evt.preventDefault();
   var hintInput = $("#hintEntry").val();
   $("#showHint").html("Hint: " + hintInput);
   $("#hintEntry").val("");
+  $("#headcenter").hide();
+  $("#torsocenter").hide();
+  $("#armleft").hide();
+  $("#armright").hide();
+  $("#legleft").hide();
+  $("#legright").hide();
 })
 
 $("#submitInput").on("click", function(evt){
   evt.preventDefault();
-  var wordInput = $("#wordEntry").val();
-  var splitWord = wordInput.split('');
-  console.log(splitWord);
-  for (i=0; i<splitWord.length; i++)
+  mysteryWord.hangmanWord = $("#wordEntry").val();
+  mysteryWord.wordArray = mysteryWord.hangmanWord.split('');
+  for (i=0; i < mysteryWord.wordArray.length; i++)
     mysteryWord.wordUnderscore[i] = " _ ";
     console.log(mysteryWord.wordUnderscore);
     $("#showWord").html(mysteryWord.wordUnderscore);
 })
+$("#wordEntry").val("");
 
 $("#submitGuess").on("click", function(evt){
   evt.preventDefault();
-  var guessInput = ("#guessEntry").val();
-
-
-
+  var guessInput = $("#guessEntry").val();
+  $("#displayGuesses").append(" <b>" + guessInput + "</b>");
+  if (mysteryWord.wordArray.includes(guessInput)) {
+    for (i=0; i< mysteryWord.wordArray.length; i++) {
+      if (mysteryWord.wordArray[i] === guessInput) {
+        mysteryWord.wordUnderscore[i] = guessInput;
+        console.log(mysteryWord.wordUnderscore);
+        $("#showWord").html(mysteryWord.wordUnderscore);
+      }
+    }
+  } else {
+    mysteryWord.counter += 1;
+    console.log(mysteryWord.counter);
+    $(mysteryWord.bodyParts[mysteryWord.counter]).show();
+    $("#guessEntry").val("");
+  }
 })
+
+//function checkGuess() {
+
+//}
+
 })
