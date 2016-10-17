39a51bedad3b82dad27fcaeda0355451a1099917
Ba Van Phan <bavphan@gmail.com>
2016-10-16 21:58:57 -0400

    fix divs

--- HangMan/script.js
+++ HangMan/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   console.log("IS IT WORKING");
 
+var mysteryWord = {
+  hangmanWord: [],
+  wordUnderscore: []
+}
+
 $("#submitInput").on("click", function(evt){
   evt.preventDefault();
   var hintInput = $("#hintEntry").val();
@@ -8,6 +13,22 @@ $("#submitInput").on("click", function(evt){
   $("#hintEntry").val("");
 })
 
+$("#submitInput").on("click", function(evt){
+  evt.preventDefault();
+  var wordInput = $("#wordEntry").val();
+  var splitWord = wordInput.split('');
+  console.log(splitWord);
+  for (i=0; i<splitWord.length; i++)
+    mysteryWord.wordUnderscore[i] = " _ ";
+    console.log(mysteryWord.wordUnderscore);
+    $("#showWord").html(mysteryWord.wordUnderscore);
+})
+
+$("#submitGuess").on("click", function(evt){
+  evt.preventDefault();
+  var guessInput = ("#guessEntry").val();
 
 
+
+})
 })
